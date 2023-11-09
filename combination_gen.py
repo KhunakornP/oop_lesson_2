1cf84468fa81cb1a3d442ec3b9337206156af1aa
“Khunakorn Pattayakorn” <khunakornp.@gmail.com>
2023-11-09 09:27:33 +0700

    gen_comb_list correct combination wrong order.

--- combination_gen.py
+++ combination_gen.py
@@ -1,3 +1,4 @@
+import copy
 def gen_comb_list(list_set):
     """
         Parameters:
@@ -11,4 +12,33 @@ def gen_comb_list(list_set):
             gen_comb_list([[1, 2, 3], [4, 5]]) returns [[1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5]]
             gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]) returns [[1, 4, 6], [2, 4, 6], [3, 4, 6], [1, 5, 6], [2, 5, 6], [3, 5, 6], [1, 4, 7], [2, 4, 7], [3, 4, 7], [1, 5, 7], [2, 5, 7], [3, 5, 7], [1, 4, 8], [2, 4, 8], [3, 4, 8], [1, 5, 8], [2, 5, 8], [3, 5, 8]]
     """
-    pass
+    if len(list_set) == 1:
+        _list = []
+        for i in list_set[0]:
+            _list.append([i])
+        return _list
+    if list_set[0] != list:
+        temp_list = []
+        for i in list_set[0]:
+            for j in list_set[1]:
+                temp_list.append([i,j])
+        list_set.pop(0)
+        list_set.pop(0)
+        list_set.insert(0, temp_list)
+    if len(list_set) >= 2:
+        temp_list = []
+        for i in list_set[0]:
+            for j in list_set[1]:
+                x = copy.deepcopy(i)
+                x.append(j)
+                temp_list.append(x)
+        list_set.pop(0)
+        list_set.pop(0)
+        list_set.insert(0, temp_list)
+        gen_comb_list(list_set)
+    return list_set[0]
+
+
+print(gen_comb_list([[1, 2, 3]]))
+print(gen_comb_list([[1, 2, 3], [4, 5]]))
+print(gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]))
